a9460d15d0ed44ce483fe458956ca0047a5d37b2
Diego Angulo Ramirez <diegoangulo5@gmail.com>
2024-11-12 17:13:51 -0400

    Added codigos

--- links.md
+++ links.md
@@ -2,5 +2,18 @@
 layout: project
 title: Links utiles
 ---
+## Codigos
+- [Binary Search](recursos/binary_search.cpp)
 
+## Temas a ver:
+- Binary Search
+- List
+- Map
+- Set
+- Flood Fill
+- Teoria de numeros
+- Recursividad y memoizacion
+
+
+## Problemas
 - [Problema piramide](recursos/piramide.pdf)
---
+++ recursos/binary_search.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/*
+    Tenemos una lista de numeros ordenada(maximo tamano 10^6), y queremos buscar un numero en ella.
+    Hay que responder K consultas, donde cada consulta indica la posicion del numero buscado
+    en la lista.
+    Si el numero no esta en la lista, se imprime -1.
+*/
+
+int n, k;
+vector<long long> lista(1000005);
+
+int buscar(long long valor) {
+    int ini = 0, fin = n - 1, mid;
+    while (true) {
+        if (ini > fin) {
+            return -1;
+        }
+        mid = (ini + fin) / 2;
+        if (lista[mid] == valor) {
+            return mid;
+        }
+        if (valor > lista[mid]) {
+            ini = mid + 1;
+        } else {
+            fin = mid - 1;
+        }
+    }
+}
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        cin >> lista[i];
+    }
+
+    cin >> k;
+    long long valor;
+    while (k--) {
+        cin >> valor;
+        cout << buscar(valor) << endl;
+    }
+
+    return 0;
+}
